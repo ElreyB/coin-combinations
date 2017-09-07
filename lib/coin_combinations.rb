03d133bcf5392236640a1cd37d87cc8e16e48471
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-07 09:24:06 -0700

    change the about of cents left for each method

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -7,25 +7,25 @@ class CoinCombinations
 
   def quarters_needed
     quarters = @cents / 25
-    @cents -= quarters
+    @cents -= quarters * 25
     quarters
   end
 
   def dimes_needed
     dimes = @cents / 10
-    @cents -= dimes
+    @cents -= dimes * 10
     dimes
   end
 
   def nickels_needed
     nickels = @cents / 5
-    @cents -= nickels
+    @cents -= nickels * 5
     nickels
   end
 
   def pennies_needed
     pennies = @cents
     @cents -= pennies
-    pennies 
+    pennies
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -8,11 +8,19 @@ describe 'CoinCombinations' do
     it 'returns the smallest amount of quarters to make change' do
       expect(coins.quarters_needed).to eq 3
     end
+
+    it 'will change the amount of cents left' do
+      expect{ coins.quarters_needed }.to change{ coins.cents }.from(89).to(14)
+    end
   end
 
   describe '#dimes_needed' do
     it 'returns the smallest amount of dimes to make change' do
       expect(coins.dimes_needed).to eq 8
+    end
+
+    it 'will change the amount of cents left' do
+      expect{ coins.dimes_needed }.to change{ coins.cents }.from(89).to(9)
     end
   end
 
@@ -20,12 +28,26 @@ describe 'CoinCombinations' do
     it 'returns the smallest amount of nickels to make change' do
       expect(coins.nickels_needed).to eq 17
     end
+
+    it 'will change the about of change left' do
+      expect{ coins.nickels_needed }.to change{ coins.cents }.from(89).to(4)
+    end
   end
 
   describe '#pennies_needed' do
     it 'returns the smallest amount of pennies to make change' do
       expect(coins.pennies_needed).to eq 89
     end
+
+    it 'will change the amount of change left' do
+      expect{ coins.pennies_needed }.to change{ coins.cents }.from(89).to(0)
+    end
   end
+
+  # describe '#to_s' do
+  #   it 'returns a user freindly output of coins needed to make change' do
+  #     expect(coins.to_s).to eq "You will need "
+  #   end
+  # end
 
 end
